b7a754dabb2d78ba3f96e009e5b42af38223f9af
vadim <vadim50>
2019-07-17 16:59:38 +0300

    Lesson 42

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,46 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+Route::group(['middleware'=>'web'],function(){
+
+	Route::match(['get','post'],'/',['uses'=>'IndexController@execute','as'=>'home']);
+	Route::get('/page/{alias}',['uses'=>'PageController@execute','as'=>'page']);
+
+	Route::auth();
+});
+
+Route::group(['prefix'=>'admin','middleware'=>'auth'],function(){
+
+	Route::get('/',function(){
+
+	});
+
+	Route::group(['prefix'=>'pages'],function(){
+
+		Route::get('/',['uses'=>'PagesController@execute','as'=>'pages']);
+		Route::match(['get','post'],'/add',['uses'=>'PagesAddController@execute','as'=>'pagesAdd']);
+		Route::match(['get','post','delete'],'/edit/{page}',['uses'=>'PagesEditController@execute','as'=>'padesEdit']);
+
+	});
+
+	Route::group(['prefix'=>'portfolios'],function(){
+
+		Route::get('/',['uses'=>'PortfolioController@execute','as'=>'portfolio']);
+		Route::match(['get','post'],'/add',['uses'=>'PortfolioAddController@execute','as'=>'portfolioAdd']);
+		Route::match(['get','post','delete'],'/edit/{portfolio}',['uses'=>'PortfolioEditController@execute','as'=>'portfolioEdit']);
+
+	});
+
+	Route::group(['prefix'=>'service'],function(){
+
+		Route::get('/',['uses'=>'ServiceController@execute','as'=>'service']);
+		Route::match(['get','post'],'/add',['uses'=>'ServicelioAddController@execute','as'=>'serviceAdd']);
+		Route::match(['get','post','delete'],'/edit/{service}',['uses'=>'ServiceEditController@execute','as'=>'serviceEdit']);
+
+	});
+
 });
