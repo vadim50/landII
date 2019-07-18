6ecba72cb3a3051f50f1ab0734ec90d168d3fc86
vadim <vadim50>
2019-07-18 17:05:40 +0300

    Lesson 44

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -3,11 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Page;
+use App\Service;
+use App\Portfolio;
+use App\People;
 
 class IndexController extends Controller
 {
     //
     public function execute(Request $request){
-    	return view('layouts.site');
+
+    	$pages = Page::All();
+    	$portfolios  = Portfolio::get(['name','filter','images']);
+    	$services = Service::where('id','<',20)->get();
+    	$peoples = People::take(3)->get();
+
+
+
+    	return view('site.index');
     }
 }
