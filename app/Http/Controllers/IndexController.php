2e80ae75eff890c3f173a450fcec597ee2ebfab6
vadim <vadim50>
2019-07-20 15:03:03 +0300

    Lesson 45

--- app/Http/Controllers/IndexController.php
+++ app/Http/Controllers/IndexController.php
@@ -18,8 +18,31 @@ class IndexController extends Controller
     	$services = Service::where('id','<',20)->get();
     	$peoples = People::take(3)->get();
 
+    	$menu = [];
+    	foreach($pages as $page){
+    		$item = ['title'=>$page->name,'alias'=>$page->alias];
+    		array_push($menu,$item);
+    	}
+
+    	$item = ['title'=>'Services','alias'=>'service'];
+    		array_push($menu,$item);
+    	$item = ['title'=>'Portfolio','alias'=>'Portfolio'];
+    		array_push($menu,$item);
+    	$item = ['title'=>'Team','alias'=>'team'];
+    		array_push($menu,$item);
+    	$item = ['title'=>'Contact','alias'=>'contact'];
+    		array_push($menu,$item);
 
 
-    	return view('site.index');
+
+    	return view('site.index',[
+
+    		'menu'=>$menu,
+    		'pages'=>$pages,
+    		'services'=>$services,
+    		'portfolios'=>$portfolios,
+    		'peoples'=>$peoples
+
+    	]);
     }
 }
